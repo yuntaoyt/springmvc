c3e94ade954b6ddf39a664458ab27adc2942bbad
yuntaoyt <13683308013@163.com>
2018-11-19 12:07:27 +0800

    Merge branch 'Test'
    # Conflicts:
    #	src/main/java/com/taotao/Test.java

--- src/main/java/com/taotao/Test.java
+++ src/main/java/com/taotao/Test.java
@@ -3,9 +3,5 @@ package com.taotao;
 public class Test {
     public static void main(String[] args) {
         System.out.println("yyyyyyyyyyyyyy");
-        System.out.println("tttttttttttttttttttttt");
-        System.out.println("yuntao");
-        System.out.println("ss");
-        System.out.println("sssssssssss");
     }
 }
