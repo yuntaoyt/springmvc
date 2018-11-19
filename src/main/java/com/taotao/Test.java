77d304595efb869f67b39430c924d6bc2d1ceab6
yuntaoyt <13683308013@163.com>
2018-11-19 12:23:14 +0800

    Merge branch 'Test'
    # Conflicts:
    #	src/main/java/com/taotao/Test.java

--- src/main/java/com/taotao/Test.java
+++ src/main/java/com/taotao/Test.java
@@ -6,6 +6,12 @@ import java.util.List;
 public class Test {
     public static void main(String[] args) {
         System.out.println("yyyyyyyyyyyyyy");
-        System.out.println("yyyyyy");
+        List  list   = new ArrayList();
+        list.add("tets");
+        list.add("yuntaio");
+        list.add("tttttt");
+        for (int i =0;i<list.size();i++){
+            System.out.println(list.get(i)+"--------------------------yyyyyyyyyyyyyyyy");
+        }
     }
 }
