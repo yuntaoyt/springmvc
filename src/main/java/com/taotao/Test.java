fd94f175f12971ff9825252a8ab71f94f943081a
yuntaoyt <13683308013@163.com>
2018-11-19 12:13:55 +0800

    Merge branch 'Test'
    # Conflicts:
    #	src/main/java/com/taotao/Test.java

--- src/main/java/com/taotao/Test.java
+++ src/main/java/com/taotao/Test.java
@@ -1,7 +1,17 @@
 package com.taotao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
     public static void main(String[] args) {
         System.out.println("yyyyyyyyyyyyyy");
+        List  list   = new ArrayList();
+        list.add("tets");
+        list.add("yuntaio");
+        list.add("tttttt");
+        for (int i =0;i<list.size();i++){
+            System.out.println(list.get(i)+"--------------------------yyyyyyyyyyyyyyyy");
+        }
     }
 }
