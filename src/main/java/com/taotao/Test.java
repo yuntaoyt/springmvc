33744425010269c26f61887ceafad7cff2ffd834
yuntaoyt <13683308013@163.com>
2018-11-25 21:35:11 +0800

    sss

--- src/main/java/com/taotao/Test.java
+++ src/main/java/com/taotao/Test.java
@@ -22,7 +22,6 @@ public class Test {
         System.out.println("ssssssssssssssssssssssssssssssssssssss");
         System.out.println("sssssssssssttttttttttttttttsststtttttttttttuuuuuuuuuu");
         System.out.println("45555555555555555555555555555555555555555555555555555555555555");
-        System.out.println("sss");
-        System.out.println("ssssytttt");
+
     }
 }
